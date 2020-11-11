4c787b23451e2270f41835693d7d9e31e5b23ea9
Scott Davis <scott@thirstyhead.com>
2020-11-10 17:11:24 -0700

    fixed colors path for vim

--- nvim/init.sh
+++ nvim/init.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.config/nvim
 cp -i dot.config-nvim-init.vim ~/.config/nvim/init.vim 
-cp -r colors ~/.config/nvim/
 
 mkdir ~/.vim
-tar xvf dot.vim.tar ~/
+tar xvf dot.vim.tar -C ~/
 cp -i dot.vim-vimrc ~/.vim/vimrc
+cp -r colors ~/.vim
